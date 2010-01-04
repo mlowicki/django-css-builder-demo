209edc884c87894a9348357473684780327e9333
Michał Łowicki <mlowicki@gmail.com>
2010-01-05 00:00:08 +0100

    add sprite_basic example

--- css_builder_demo/development.py
+++ css_builder_demo/development.py
@@ -1,5 +1,7 @@
 from css_builder_demo.settings import *
 
+DEBUG = True
+
 INSTALLED_APPS += ('css_builder',)
 
 DATABASE_ENGINE = 'mysql'
@@ -10,3 +12,8 @@ DATABASE_HOST = ''
 DATABASE_PORT = ''
 
 CSS_BUILDER_SOURCE = here(('media', 'internal',))
+CSS_BUILDER_SPRITES = {
+    'sprite_1': {
+                'files':['opera.png', 'ff.png', 'safari.png'],
+                'orientation': 'vertically'}
+}
--- css_builder_demo/urls.py
+++ css_builder_demo/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 urlpatterns = patterns('django.views.generic.simple',
     (r'^2b64_basic/$', 'direct_to_template', {'template': '2b64_basic.html'}),
+    (r'^sprite_basic/$', 'direct_to_template',
+     {'template': 'sprite_basic.html'}),
 )
 
 if settings.DEBUG:
