ae5a4f38d31ab1013b19e4a9fec3f0e505dba725
Michał Łowicki <mlowicki@gmail.com>
2010-01-04 21:13:32 +0100

    basic setup

---
+++ css_builder_demo/development.py
@@ -0,0 +1,10 @@
+from css_builder_demo.settings import *
+
+INSTALLED_APPS += ('css_builder',)
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'django_css_builder_demo'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'root'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
